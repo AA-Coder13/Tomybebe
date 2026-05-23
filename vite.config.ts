6a68978acbd69a6ea3463a1745b2a8dea383abd2
AA-Coder13 <xywtos@gmail.com>
2026-05-23 11:10:20 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Tomybebe/',
+    base: 'Tomybebe/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
